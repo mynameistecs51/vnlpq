b59fe2e96c2f86b44c4cbefcdefcde5b9e339cd3
chaiwat <mynameistecs51@gmail.com>
2016-07-21 10:49:09 +0700

    update

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,3 +1,164 @@
+<<<<<<< HEAD
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('mdl_main');
+	}
+	public function index()
+	{
+		$data = array(
+			'fileDownload' => $this->mdl_main->getFile(),
+			'news' => $this->mdl_main->getNews(),
+			);
+		$this->load->view('index',$data);
+	}
+	public function databegin()  //ข้อมูลเบื้องต้นโครงการ
+	{
+		$this->load->view('begindata');
+	}
+	public function consultant()  //คณะที่ปรึกษา
+	{
+		$this->load->view('consultant');
+	}
+	public function endorsement() //คณะรัยรอง
+	{
+		$this->load->view('endorsement');
+	}
+	public function working() //คณะทำงาน
+	{
+		$this->load->view('working');
+	}
+	public function workingtime() //กรอบระยะเวลาดำเนินงาน
+	{
+		$this->load->view('workingtime');
+	}
+	public function workplan() //แผนระยะเวลาดำเนินงาน
+	{
+		$this->load->view('workplan');
+	}
+	public function management()
+	{
+		$this->load->view('management');
+	}
+	public function upload()
+	{
+		$data = array(
+			'error' => '',
+			);
+		$this->load->view('admin/docUpload',$data);
+	}
+	public function docUpload()
+	{
+		// print_r($_FILES);
+		if($_FILES['userfile'] == ""){
+			$massage = "กรุณาเลือกไฟล์ที่จะอัพโหลด";
+			$url = "main/upload";
+			$this->alert($massage,$url);
+			// $this->load->view('admin/docUpload');
+		}else{
+			// echo "<pre>";
+			// print_r($_FILES);
+			$this->mdl_main->upload_fileDoc();
+		}
+	}
+	public function download($file_name){		// download file project
+
+		$data = file_get_contents('./files/docUpload/'.$file_name);
+		$name = $file_name;
+		echo force_download($name,$data);
+		redirect('main','refresh');
+	}
+	public function addNews()
+	{
+		$titleNews = $this->input->post('titleNews');
+		$newsContent = $this->input->post('newsContent');
+		if($titleNews != '' & $newsContent != ''){
+			$this->mdl_main->addNews($titleNews,$newsContent);
+		}else{
+			$this->load->view('admin/addNews');
+		}
+	}
+	public function readNews($newsID)
+	{
+		// $data = array(
+		// 	'readNews' => $this->mdl_main->getNewsID($newsID),
+		// 	);
+		// $this->load->view('readNews',$data);
+		switch ($newsID) {
+			case '4':
+			$this->load->view('readNews1');
+			break;
+			case '5':
+			$this->load->view('readNews2');
+			break;
+			case '6':
+			$this->load->view('readNews3');
+			break;
+			case '7':
+			$this->load->view('readNews4');
+			break;
+			case '8':
+			$this->load->view('readNews5');
+			break;
+			case '9':
+			$this->load->view('readNews6');
+			break;
+			case '10':
+			$this->load->view('readNews7');
+			break;
+			case '11':
+			$this->load->view('readNews8');
+			break;
+			case '12':
+				$this->load->view('readNews9');
+				break;
+			case '13':
+				$this->load->view('readNews10');
+				break;
+			default:
+			$massage ="กำลังปรับปรุงเนื้อหาข่าว..";
+			$url = "main/";
+			$this->alert($massage,$url);
+			break;
+		}
+	}
+	function addPicture(){
+
+		if($_FILES  != null){
+			$upload_research = $this->Model_main->insert_Research('file_research',$_FILES['file_research']);
+			$name ="";
+			$a = explode(',',$upload_research);
+			for($i = 1 ; $i < count($a) ; $i++){
+				$name .= $a[$i].',';
+			}
+			$insert_research = array(
+				'res_name' => $this->input->post('input_resName'),
+				'res_file' => $a[0],
+				'res_pict' => substr($name,0,-1),
+				'res_detail' => $this->input->post('input_docDetail'),
+				'res_type' => $this->input->post('research_type'),
+				);
+			$this->db->insert('research',$insert_research);
+
+		}else{
+			$this->load->view('admin/addPicture');
+		}
+	}
+	public function alert($massage, $url)
+	{
+		echo "<meta charset='UTF-8'>
+		<SCRIPT LANGUAGE='JavaScript'>
+			window.alert('$massage')
+			window.location.href='".site_url($url)."';
+		</SCRIPT>";
+	}
+}
+
+/* End of file welcome.php */
+=======
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Main extends CI_Controller {
@@ -151,4 +312,5 @@ class Main extends CI_Controller {
 }
 
 /* End of file welcome.php */
+>>>>>>> eb12f0d4178c9968f12647d31619649962c7d389
 /* Location: ./application/controllers/welcome.php */
---
+++ application/views/readNews10.php
@@ -0,0 +1,37 @@
+<?php $this->load->view('header');?>
+<div class="col-sm-12"><br/><br/></div>
+<div class="row">
+	<div class="col-md-12">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4><i class="fa fa-file-text-o"></i>FM 100.5 MCOT NEWS NETWORK : มหาวิทยาลัยราชภัฏอุดรธานี จัดทำมาตรฐานอาชีพและคุณวุฒิวิชาชีพ สาขาวิชาชีพผู้สอนภาษาเวียดนาม
+				</h4>
+			</div>
+			<div class="panel-body">
+
+				<div class="col-sm-12">
+					<div class="col-md-12">
+						<a href="#" class="thumbnail">
+							<img src="<?php echo base_url().'img/pictureNews/fm100-5.png'; ?>" alt="header"  >
+						</a>
+					</div>
+					<div class="col-sm-8">
+						<audio controls autoplay class="pull-right">
+							<source src="<?php echo base_url().'files/docUpload/130716093855.mp3';?>" type="audio/mpeg">
+							</audio>
+						</div>
+					</div>
+					<p style="text-align:justify;">
+						<b>อุดรธานี - มรภ.อุดรธานี จับมือสถาบันวิจัยและพัฒนา และสถาบันคุณวุฒิวิชาชีพ เตรียมขับเคลื่อนสร้างมาตรฐานอาชีพผู้สอนภาษาภาษาเวียดนาม</b><br>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						สัมภาษณ์ : ผศ.ดร.กฤษฎา ณ หนองคาย ผอ.สถาบันวิจัยและพัฒนา มหาวิทยาลัยราชภัฏ (มรภ.) อุดรธานี เรื่อง "มหาวิทยาลัยราชภัฏ (มรภ.) อุดรธานี จัดทำมาตรฐานอาชีพและคุณวุฒิวิชาชีพ สาขาวิชาชีพผู้สอนภาษาเวียดนาม" รายการ Good Morning ASEAN โดย วีรวรรณ วรวุตม์ และ บวรพงษ์ วัฒนาธนากุล และช่วงเวลา 06.30 พบกับ ทรงฤทธิ์ โพนเงิน
+
+						<br/>
+						<b>ที่มา:</b> <?php echo anchor('http://mcot-web.mcot.net/fm1005/stream.php?id=578452439381632c548b45ab#.V5A4M-iLTIX','FM 100.5 MCOT NEWS NETWORK');?>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr/>
+	<?php $this->load->view('footer');?>
